37a66e6c28cc64f04720b25faf6c2d723652f255
Chris Yang <chrisyx511@gmail.com>
2023-05-01 16:00:37 -0400

    Collision Refactor, split, multiplayer

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // Super Smash eachother in the ass Brothers - Chris YANG et Felix WU
 // Concentration Informatique - 2023-03-31
 
-import { getNewXPos, getNewYPos, physicalConstants } from "./modules/physicalObject.js"
+import Player, { physicalConstants, keysDown, keysBlocked} from "./modules/player.js"
 
 
 const canvas = document.createElement("canvas")
@@ -18,8 +18,7 @@ canvas.style.margin = "auto"
 // Movement and player
 let activeArea = {}
 /*deplacement du joueur*/
-let keysDown = []
-let keysBlocked = []
+
 
 canvas.style.backgroundRepeat = "no-repeat"
 canvas.style.backgroundSize = "cover"
@@ -36,88 +35,76 @@ document.addEventListener("keyup", (e) => {
 const battefield = {
     name: "Battlefield",
     bgPath: "./assets/SSBB_Battlefield_Stage.webp",
-    basePlatforms: [
+    platforms: [
         {
             x: 192,
             y:484,
             w: 900,
-            h: 70
-        }
-    ]
+            h: 70,
+            basePlatform: true
+        },
+        {
+            x: 305,
+            y: 345,
+            w: 195,
+            h: 15,
+            basePlatform: false
+        },
+        {
+            x: 783,
+            y: 345,
+            w: 195,
+            h: 15,
+            basePlatform: false
+        },        {
+            x: 543,
+            y: 215,
+            w: 195,
+            h: 15,
+            basePlatform: false
+        },
+    ],
+    players: []
 }
 
+function decomposeVector(norme, orientationInRad) {
+    return {
+        x: norme*Math.cos(orientationInRad),
+        y: norme*Math.sin(orientationInRad)
+    }
+}
+
+function handleCollision(character, platform) {
+    if ( character.position.x + character.position.w > platform.x &&
+         character.position.x < platform.x + platform.w) {
+        if ( character.position.y < platform.y && character.position.y + character.position.h >= platform.y &&
+             character.position.y + character.position.h < platform.y + platform.h ) {
+            character.movementY.speed = 0
+            character.movementY.jumpCount = 0
+            character.position.y = platform.y - character.position.h
+
+        }
+        if ( character.position.y + character.position.h > platform.y + platform.h && character.position.y <= platform.y + platform.h &&
+             platform.basePlatform === true) {
+            if (character.movementY.speed < 0) {
+                character.movementY.speed = -character.movementY.speed
+            }
+        }
+    }
+}
 
 
 //
-class Player {
-    position = {
-        x: 500,
-        y:  0,
-        w:  70,
-        h:  85
-    }
 
-    movementX = {
-        speed: 0,
-        accel: 0
 
-    }
 
-    movementY = {
-        speed: 0,
-        accel: 0,
-        jumpCount: 0
-    }
-    mass = 10
-    characterMaxSpeed = 5
-    percentage =  0
-    maxJumpCount = 2
-    handleMovement(canvasObject = canvas, contextObject = ctx) {
-        const self = this
-        if("ArrowLeft" in keysDown) {
-            if (self.movementX.speed <= -self.characterMaxSpeed) {
-                self.movementX.accel = -physicalConstants.x_deceleration
-            } else {
-                self.movementX.accel = -0.8
-            }
-            
-            console.log(self.movementX)
-        } else if("ArrowRight" in keysDown) {
-            if (self.movementX.speed >= self.characterMaxSpeed) {
-                self.movementX.accel = physicalConstants.x_deceleration
-            } else {
-                self.movementX.accel = 0.8
-            }
-            console.log(self.movementX)
-        } else {
-            self.movementX.accel = 0
-        }
-        console.log(keysDown)
-        
-        if ("ArrowUp" in keysDown) {
-            if (typeof keysBlocked["ArrowUp"] === 'undefined' && self.movementY.jumpCount <= self.maxJumpCount) {
-                self.movementY.speed = -6.5
-                self.movementY.jumpCount++
-            } 
-            keysBlocked["ArrowUp"] = true
-        }
-
-        getNewXPos(self.position, self.movementX)
-        getNewYPos(self.position, self.movementY)
-
-    
-        
-    }
-    draw(contextObject = ctx) {
-        let x = this.position.x
-        let y = this.position.y
-        let w = this.position.w
-        let h = this.position.h
-        contextObject.fillRect(x, y, w, h)
-    }
-}
 
 const player1 = new Player
+const player2 = new Player
+player2.position.x = 1000
+player2.controlSetNumber = 1
+loadStage(battefield)
+activeArea.players.push(player1, player2)
 //
 function getCursorPosition(canvas, event) {
     const rect = canvas.getBoundingClientRect()
@@ -143,33 +130,19 @@ if (debugMode) {
     targetCanvas.style.backgroundImage = `url(${areaContainer.bgPath})`
 }
 
-/**
- * Collision between objects
- * @param {Object} objet1 
- * @param {Object} objet2 
- * @returns 
- */
- function collision(objet1 = null, objet2 = null){
-    if (!objet1 || !objet2) {
-        return null
-    }
-	if (objet1.x + objet1.w >= objet2.x &&
-        objet1.x <= objet2.x + objet2.w && 
-        objet1.y + objet1.h >= objet2.y && 
-        objet1.y <= objet2.y + objet2.h) {
-		return true
-	}
-}
-
-loadStage(battefield)
 
 function main() {
     ctx.clearRect(0,0, canvas.width, canvas.height)
-    for (let i = 0; i < activeArea.basePlatforms.length; i++) {
-        ctx.fillRect(activeArea.basePlatforms[i].x,activeArea.basePlatforms[i].y,activeArea.basePlatforms[i].w,activeArea.basePlatforms[i].h)
+    for (let i = 0; i < activeArea.platforms.length; i++) {
+        //ctx.fillRect(activeArea.platforms[i].x,activeArea.platforms[i].y,activeArea.platforms[i].w,activeArea.platforms[i].h)
+        for (let j = 0; j < activeArea.players.length; j++) {
+            handleCollision(activeArea.players[j], activeArea.platforms[i])
+        }
     }
     player1.draw(ctx)
     player1.handleMovement()
+    player2.draw(ctx)
+    player2.handleMovement()
     requestAnimationFrame(main)
 
 }
