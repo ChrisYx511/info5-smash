7b74b7a8ae2cb910600b90cbd18c10bed4fcbef2
Chris Yang <chrisyx511@gmail.com>
2023-05-24 16:00:50 -0400

    started characterselect

--- script/htmlHelpers.js
+++ script/htmlHelpers.js
@@ -1,22 +1,72 @@
+const wiiMenuStartButton = document.getElementById("wiiMenuStartButton")
+const gameContainerSplashMenu = document.getElementById('gameContainerSplashMenu')
+const gameContainerBrawlMenu = document.getElementById("gameContainerBrawlMenu")
+
+const menuMusic = new Audio("./assets/sound/music/menu.m4a")
+
 const wiiMenuVideo = document.querySelector("#gameContainerWiiMenu video")
-const introVideo = document.querySelector('#gameContainerIntroVideo video')
+const introVideo = document.querySelector('#gameContainerSplashMenu video')
+
+let menuProgress = 0
 
 function startWiiMenu() {
     document.getElementById('gameContainerWiiMenuSplash').style.display = 'none'
     document.getElementById('gameContainerWiiMenu').style.display = 'inherit'
     wiiMenuVideo.play()
+    wiiMenuStartButton.style.display = "none"
     wiiMenuVideo.addEventListener("ended", ()=>{
         wiiMenuVideo.pause()
         wiiMenuVideo.style.display = "none"
+        wiiMenuStartButton.style.display = "inherit"
     })
+    menuProgress++
 }
 
 function loadIntroVideo() {
+    menuProgress++
     document.getElementById('gameContainerWiiMenu').style.display = 'none'
-    document.getElementById('gameContainerIntroVideo').style.display = 'inherit'
-    introVideo.play()
-    introVideo.addEventListener("ended", () => {
-        introVideo.pause()
-        introVideo.style.display = "none"
+    gameContainerSplashMenu.style.display = "inherit"
+    const abortIntro = new AbortController
+    setTimeout(() => {
+        introVideo.play()
+        gameContainerSplashMenu.addEventListener("click", () => {
+            switch (menuProgress){
+                case 2:
+                    introVideo.pause()
+                    introVideo.style.display = "none"
+                    menuProgress++
+                    break;
+                case 3: 
+                    abortIntro.abort()
+                    loadMainMenu()
+                    break;
+            }
+        }, {signal: abortIntro.signal})
+        document.addEventListener("keydown", () => {
+            switch (menuProgress){
+                case 2:
+                    introVideo.pause()
+                    introVideo.style.display = "none"
+                    menuProgress++
+                    break;
+                case 3: 
+                    abortIntro.abort()
+                    loadMainMenu()
+                    break;
+            }
+        }, {signal: abortIntro.signal})
+        introVideo.addEventListener("ended", () => {
+            introVideo.pause()
+            introVideo.style.display = "none"
+        })
     })
+
+}
+
+function loadMainMenu() {
+    gameContainerSplashMenu.style.display = "none"
+    gameContainerBrawlMenu.style.display = "inherit"
+    menuProgress++
+    menuMusic.play()
+    menuMusic.loop = true
 }
